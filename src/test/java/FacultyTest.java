a0123d97e336683f9c485f769c642155bdeac053
Roman Kuite <roman.kuite@gmail.com>
2021-01-21 15:51:24 +0100

    Add Faculty Check and method

---
+++ src/test/java/FacultyTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FacultyTest {
+
+    @ParameterizedTest
+    @MethodSource
+
+    public void FacultyTestFunction(int number, int expected){
+        int result = FacultyApp.FacultyTest(number);
+        assertEquals(expected, result);
+    }
+    private static Stream<Arguments>FacultyTestFunction(){
+        return Stream.of(
+                Arguments.of(0,1),
+                Arguments.of(1,1),
+                Arguments.of(2,2),
+                Arguments.of(3,6)
+        );
+    }
+}
