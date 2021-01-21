3a49e2f170aa8fc4bece7f0ec692420a2bbd8898
Roman Kuite <roman.kuite@gmail.com>
2021-01-21 11:16:08 +0100

    Add second Test including if statement

---
+++ src/main/java/SchleifenTest.java
@@ -0,0 +1,14 @@
+public class SchleifenTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String checkPeople (int people) {
+        if (people > 30) {
+            return "There are more than 30 people in the room. Shame on you!";
+        } else {
+            return "You're good to go!";
+        }
+    }
+}
